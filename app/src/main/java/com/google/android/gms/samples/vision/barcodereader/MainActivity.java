0744a98d0c0d7b68d4bf397250f4b82233d6d9f8
Artem Solod <solod_a@ukr.net>
2018-04-23 10:50:04 +0300

    added sending by email

--- app/src/main/java/com/google/android/gms/samples/vision/barcodereader/MainActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/MainActivity.java
@@ -16,12 +16,20 @@
 
 package com.google.android.gms.samples.vision.barcodereader;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -30,6 +38,10 @@ import android.widget.TextView;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Stack;
 
@@ -47,6 +59,109 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final int RC_BARCODE_CAPTURE = 9001;
     private static final String TAG = "BarcodeMain";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
+    private Uri photoURI = null;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        String mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+                if (rc != PackageManager.PERMISSION_GRANTED) {
+                    requestCameraPermission();
+                }
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    public Intent createEmailOnlyChooserIntent(Intent source,
+                                               CharSequence chooserTitle) {
+        Stack<Intent> intents = new Stack<Intent>();
+        Intent i = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",
+                "info@domain.com", null));
+        List<ResolveInfo> activities = getPackageManager()
+                .queryIntentActivities(i, 0);
+
+        for (ResolveInfo ri : activities) {
+            Intent target = new Intent(source);
+            target.setPackage(ri.activityInfo.packageName);
+            intents.add(target);
+        }
+
+        if (!intents.isEmpty()) {
+            Intent chooserIntent = Intent.createChooser(intents.remove(0),
+                    chooserTitle);
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                    intents.toArray(new Parcelable[intents.size()]));
+
+            return chooserIntent;
+        } else {
+            return Intent.createChooser(source, chooserTitle);
+        }
+    }
+
+    private  void dispatchSendEmail() {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("*/*");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"solod_a@ukr.net"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Crash report");
+        i.putExtra(Intent.EXTRA_TEXT, "Some crash report details");
+        i.putExtra(Intent.EXTRA_STREAM, photoURI);
+        startActivity(createEmailOnlyChooserIntent(i, "Send via email"));
+    }
+
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions,
+                        RC_HANDLE_CAMERA_PERM);
+            }
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,43 +179,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.send_email).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("*/*");
-//                i.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(crashLogFile));
-                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"solod_a@ukr.net"});
-                i.putExtra(Intent.EXTRA_SUBJECT, "Crash report");
-                i.putExtra(Intent.EXTRA_TEXT, "Some crash report details");
-
-                startActivity(createEmailOnlyChooserIntent(i, "Send via email"));
+                dispatchTakePictureIntent();
             }
 
-            public Intent createEmailOnlyChooserIntent(Intent source,
-                                                       CharSequence chooserTitle) {
-                Stack<Intent> intents = new Stack<Intent>();
-                Intent i = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",
-                        "info@domain.com", null));
-                List<ResolveInfo> activities = getPackageManager()
-                        .queryIntentActivities(i, 0);
 
-                for (ResolveInfo ri : activities) {
-                    Intent target = new Intent(source);
-                    target.setPackage(ri.activityInfo.packageName);
-                    intents.add(target);
-                }
-
-                if (!intents.isEmpty()) {
-                    Intent chooserIntent = Intent.createChooser(intents.remove(0),
-                            chooserTitle);
-                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
-                            intents.toArray(new Parcelable[intents.size()]));
-
-                    return chooserIntent;
-                } else {
-                    return Intent.createChooser(source, chooserTitle);
-                }
-            }
         });
-        }
+    }
 
     /**
      * Called when a view has been clicked.
@@ -161,6 +245,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            System.out.println(photoURI);
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+          //  mImageView.setImageBitmap(imageBitmap);
+            dispatchSendEmail();
         }
     }
 }
